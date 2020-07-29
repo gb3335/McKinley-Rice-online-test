1644b938341d33d1a8afb4320519989b35f57b34
giriraj parihar <giriraj.parihar2019@vitstudent.ac.in>
2020-07-29 18:25:56 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# ReactChat
+## stap 1. yarn install
+## stap 2. yarn start
+
+## for installing yarn: npm install yarn
---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+    domain: 'gb3335.us.auth0.com',
+    clientId: 'tAWcpN21egRhtKNEtunrXxy67XpcgEbH',
+    callbackUrl: 'http://localhost:3000/callback' //Callback URL set in the Auth0 dasbhoard.
+}
